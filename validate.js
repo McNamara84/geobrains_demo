ce14e7a881a2fced7e42fe31e4aff986c3dca2cd
McNamara <holgerehrmann@gmail.com>
2024-02-12 15:12:45 +0100

    Funktion zur Echtzeitvalidierung verbessert

--- validate.js
+++ validate.js
@@ -1,117 +1,101 @@
-//Echtzeitvalidierung aller Felder.
-//1. Validierung alle Felder in Resource Information
-//1.A. Validierung das Feld "Doi"
 $(document).ready(function () {
-    function validateDoi(doi) {
-        var isValidDoi = /^10\.[0-9]{0,4}\/GFZ\.[0-9]{1}.[0-9]{1}.[0-9]{4}.[0-9]{3}$/.test(doi) || doi === "";
-        if (isValidDoi) {
-            $('#inputDOI').css("background-color", "#ffffff");
-        }
-        else {
-            $('#inputDOI').css("background-color", "#FDD8DD");
-        }
-    }
-    $("#inputDOI").on("input", function () {
-        var doi = $(this).val();
-        validateDoi(doi);
+  // Validierungsfunktionen, die die Klasse 'invalid' hinzufügen (wenn Einagbe korrekt) oder entfernt (wenn Eingabe falsch ist)
+  function validateDoi(doi) {
+    var isValidDoi = /^10\.[0-9]{0,4}\/GFZ\.[0-9]{1}.[0-9]{1}.[0-9]{4}.[0-9]{3}$/.test(doi) || doi === "";
+    $("#inputDOI").toggleClass("invalid", !isValidDoi);
+  }
+
+  function validateYear(year) {
+    var isValidYear = /^[0-9]{4}$/.test(year) || year === "";
+    $("#inputPublicationYear").toggleClass("invalid", !isValidYear);
+  }
+
+  function validateVersion(version) {
+    var isValidVersion = /^[0-9]{1,2}$/.test(version) || version === "";
+    $("#inputVersion").toggleClass("invalid", !isValidVersion);
+  }
+
+  function validateTitle(title) {
+    var isValidTitle = title.trim() !== "";
+    $("#inputTitle").toggleClass("invalid", !isValidTitle);
+  }
+
+  function validateLastname(lastname) {
+    var isValidLastname = /^[a-zA-ZäöüÄÖÜß]+$/.test(lastname) || lastname === "";
+    $("#inputLastname").toggleClass("invalid", !isValidLastname);
+  }
+
+  function validateFirstname(firstname) {
+    var isValidFirstname = /^[a-zA-ZäöüÄÖÜß]+$/.test(firstname) || firstname === "";
+    $("#inputFirstname").toggleClass("invalid", !isValidFirstname);
+  }
+
+  function validateORCID(orcid) {
+    var isValidORCID = /^[0-9]{4}-[0-9]{4}-[0-9]{4}-([0-9]{4}|[0-9]{3}X)$/.test(orcid) || orcid === "";
+    $("#inputAuthorORCID").toggleClass("invalid", !isValidORCID);
+  }
+
+  function validateAffiliation(affiliation) {
+    var isValidAffiliation = affiliation.trim() !== "";
+    $("#inputAuthorAffiliation").toggleClass("invalid", !isValidAffiliation);
+  }
+
+  // Felder bei Eingabe validieren
+  $("#inputDOI").on("input", function () {
+    validateDoi(this.value);
+  });
+  $("#inputPublicationYear").on("input", function () {
+    validateYear(this.value);
+  });
+  $("#inputVersion").on("input", function () {
+    validateVersion(this.value);
+  });
+  $("#inputTitle").on("input", function () {
+    validateTitle(this.value);
+  });
+  $("#inputLastname").on("input", function () {
+    validateLastname(this.value);
+  });
+  $("#inputFirstname").on("input", function () {
+    validateFirstname(this.value);
+  });
+  $("#inputAuthorORCID").on("input", function () {
+    validateORCID(this.value);
+  });
+  $("#inputAuthorAffiliation").on("input", function () {
+    validateAffiliation(this.value);
+  });
+
+  // Überprüfen, ob der Submit-Button aktiviert oder deaktiviert werden soll
+  function checkFormValidity() {
+    var allFieldsFilled = true;
+    var requiredFields = [
+      "#inputDOI",
+      "#inputPublicationYear",
+      "#inputVersion",
+      "#inputTitle",
+      "#inputLastname",
+      "#inputFirstname",
+      "#inputAuthorORCID",
+      "#inputAuthorAffiliation",
+    ];
+
+    // Überprüfen, ob alle Pflichtfelder ausgefüllt sind
+    requiredFields.forEach(function (selector) {
+      if ($(selector).val().trim() === "") {
+        allFieldsFilled = false;
+      }
     });
-    //1.B. Validierung das Feld "Publication Year"
-    function validateYear(year) {
-        var isValidYear = /^[0-9]{4}$/.test(year) || year === "";
-        if (isValidYear) {
-            $('#inputPublicationYear').css("background-color", "#ffffff");
-        }
-        else {
-            $('#inputPublicationYear').css("background-color", "#FDD8DD");
-        }
-    }
-    $('#inputPublicationYear').on("input", function () {
-        var year = $(this).val();
-        validateYear(year);
-    });
-    //1.C. Validierung das Feld "Version"
-    function validateVersion(version) {
-        var isValidVersion = /^[0-9]{1,2}$/.test(version) || version === "";
-        if (isValidVersion) {
-            $('#inputVersion').css("background-color", "#ffffff");
-        }
-        else {
-            $('#inputVersion').css("background-color", "#FDD8DD");
-        }
-    }
-    $('#inputVersion').on("input", function () {
-        var version = $(this).val();
-        validateVersion(version);
-    });
-    //1.D. Validierung das Feld "Title"
-    function validateTitle(title) {
-        var isValidTitle = /^.*\b\w+\b.*$/.test(title) || title === "";
-        if (isValidTitle) {
-            $('#inputTitle').css("background-color", "#ffffff");
-        }
-        else {
-            $('#inputTitle').css("background-color", "#FDD8DD");
-        }
-    }
-    $('#inputTitle').on("input", function () {
-        var Title = $(this).val();
-        validateTitle(Title);
-    });
-    //2. Validierung alle Felder in Authors
-    //2.A. Validierung das Feld "Lastname"
-    function validateLastname(lastname) {
-        var isValidLastname = /^[a-zA-ZäöüÄÖÜß]+$/.test(lastname) || lastname === "";
-        if (isValidLastname) {
-            $('#inputLastname').css("background-color", "#ffffff");
-        }
-        else {
-            $('#inputLastname').css("background-color", "#FDD8DD");
-        }
-    }
-    $('#inputLastname').on("input", function () {
-        var Lastname = $(this).val();
-        validateLastname(Lastname);
-    });
-    //2.B. Validierung das Feld "Firstname"
-    function validateFirstname(firstname) {
-        var isValidFirstname = /^[a-zA-ZäöüÄÖÜß]+$/.test(firstname) || firstname === "";
-        if (isValidFirstname) {
-            $('#inputFirstname').css("background-color", "#ffffff");
-        }
-        else {
-            $('#inputFirstname').css("background-color", "#FDD8DD");
-        }
-    }
-    $('#inputFirstname').on("input", function () {
-        var Firstname = $(this).val();
-        validateFirstname(Firstname);
-    });
-    //2.C. Validierung das Feld "Author ORCID"
-    function validateORCID(orcid) {
-        var isValidORCID = /^[0-9]{4}-[0-9]{4}-[0-9]{4}-([0-9]{4}|[0-9]{3}X)$/.test(orcid) || orcid === "";
-        if (isValidORCID) {
-            $('#inputAuthorORCID').css("background-color", "#ffffff");
-        }
-        else {
-            $('#inputAuthorORCID').css("background-color", "#FDD8DD");
-        }
-    }
-    $('#inputAuthorORCID').on("input", function () {
-        var ORCID = $(this).val();
-        validateORCID(ORCID);
-    });
-    //2.D. Validierung das Feld "Affiliation"
-    function validateAffiliation(affiliation) {
-        var isValidAffiliation = /^.*\b\w+\b.*$/.test(affiliation) || affiliation === "";
-        if (isValidAffiliation) {
-            $('#inputAuthorAffiliation').css("background-color", "#ffffff");
-        }
-        else {
-            $('#inputAuthorAffiliation').css("background-color", "#FDD8DD");
-        }
-    }
-    $('#inputTitle').on("input", function () {
-        var Affiliation = $(this).val();
-        validateAffiliation(Affiliation);
-    });
+
+    // Prüfen, ob das Formular valide ist (keine invaliden Felder und alle Pflichtfelder ausgefüllt)
+    var isFormValid = allFieldsFilled && $(".invalid").length === 0;
+
+    $("#submitButton").prop("disabled", !isFormValid);
+  }
+
+  // Überprüfung auslösen, wenn sich Eingabefelder ändern
+  $("input").on("input", checkFormValidity);
+
+  // Initialer Aufruf, um den Anfangszustand des Submit-Buttons zu setzen
+  checkFormValidity();
 });
