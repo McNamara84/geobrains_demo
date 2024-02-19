0a1c74a8ff30ca53577c8d830a3f6cd4aa7b5a4c
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2024-02-19 11:45:33 +0100

    Das Eingabefeld "Role" vergrößert

--- select.js
+++ select.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-    $('.multiple-select').select2({
-        placeholder: 'Choose Role...'
+    $('#inputRole').select2({
+        //width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
+        placeholder: 'Choose Role...',
+        theme: 'bootstrap-5',
+        //dropdownParent: $('.multiple-select').parent(),
     });
 });
