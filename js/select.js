50c0095a2156acbdd4ab56c650b7b4eac39d5c8e
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2024-03-21 12:21:28 +0100

    Select2 entfernt

--- js/select.js
+++ js/select.js
@@ -1,8 +1,2 @@
 $(document).ready(function () {
-    $('#inputRole').select2({
-        //width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
-        placeholder: 'Choose Role...',
-        theme: 'bootstrap-5',
-        //dropdownParent: $('.multiple-select').parent(),
-    });
 });
